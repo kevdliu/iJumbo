66a178484068e480759128b4712c2afea0760afc
kevdliu <kevdliu@gmail.com>
2015-09-19 16:43:39 -0400

    main menu

--- app/src/main/java/com/ijumbo/ijumbo/HomeAdapter.java
+++ app/src/main/java/com/ijumbo/ijumbo/HomeAdapter.java
@@ -1,17 +1,24 @@
 package com.ijumbo.ijumbo;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class HomeAdapter extends BaseAdapter {
     private Context mContext;
     private final String[] mTitles = {"News", "Locations", "Dining", "Events", "Transportation", "Links"};
-    private final int[] mImg = {};
+    private final int[] mImg = {R.mipmap.news, R.mipmap.locations, R.mipmap.dining, R.mipmap.events, R.mipmap.transportation, R.mipmap.links};
 
     public HomeAdapter(Context c) {
         mContext = c;
@@ -40,15 +47,21 @@ public class HomeAdapter extends BaseAdapter {
 
         if (convertView == null) {
             grid = new View(mContext);
-            grid = inflater.inflate(R.layout.row_main, null);
+            grid = inflater.inflate(R.layout.tile_main, null);
         } else {
             grid = (View) convertView;
         }
 
+        LinearLayout ll = (LinearLayout) grid.findViewById(R.id.ll);
         TextView textView = (TextView) grid.findViewById(R.id.title);
         ImageView imageView = (ImageView)grid.findViewById(R.id.img);
         textView.setText(mTitles[position]);
         imageView.setImageResource(mImg[position]);
+        ll.setBackgroundColor(Color.argb(150, 150, 150, 150));
+
+        View activityContent = ((Activity) mContext).getWindow().findViewById(Window.ID_ANDROID_CONTENT);
+        int titleHeight = activityContent.getBottom() - activityContent.getTop();
+        grid.setMinimumHeight(titleHeight / 3);
 
         return grid;
     }
