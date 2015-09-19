425c4df8bcf9dea7f11df682fa382b33fdd3f338
ecokova <elena.cokova@tufts.edu>
2015-09-19 15:14:19 -0400

    project?

--- app/src/main/java/com/ijumbo/ijumbo/HomeActivity.java
+++ app/src/main/java/com/ijumbo/ijumbo/HomeActivity.java
@@ -4,14 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.util.ArrayList;
+import java.util.List;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends Activity {
+        List<String> list;
+        GridView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        list=new ArrayList<String>();
+        grid=(GridView) findViewById(R.id.gridView1);
+
+        list.add("Grid 1");
+        list.add("Grid 2");
+        list.add("Grid 3");
+        list.add("Grid 4");
+        list.add("Grid 5");
+        list.add("Grid 6");
+        list.add("Grid 7");
+        list.add("Grid 8");
+        list.add("Grid 9");
     }
 
     @Override
